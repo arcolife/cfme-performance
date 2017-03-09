ff7a1486098077b2d12680b006e7e743e9fb36e2
Archit Sharma <arcsharm@redhat.com>
2017-03-09 22:46:47 +0530

    add script to generate report from collectd data

---
+++ cfme-performance/lib/postprocess/generate_report_from_results.py
@@ -0,0 +1,17 @@
+#!/bin/env python3
+
+# This module generates a report which includes TLC from appliances,
+# as well as embedded grafana panels.
+
+import sys
+import requests
+
+# url = 'http://perf-carbon.perf.lab.eng.rdu.redhat.com:9000/render"
+# cloud = sys.argv[1]
+# node = sys.argv[2]
+
+class ReportGenerator(object):
+    '''
+    '''
+    def __init__(self):
+        pass
